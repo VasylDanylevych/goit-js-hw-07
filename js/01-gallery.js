e39acaf9ece44400c432ef83692cc3d958c9cd49
VasylDanylevych <ukrainec31@gmail.com>
2023-01-10 15:30:41 +0200

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryList.addEventListener("click", onGalleryLinkClick);
+
+function createGalleryMarkup (gallery) {
+    return gallery.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+            <a href="${original}" class="gallery__link">
+                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+            </a>
+        </div>`
+    }).join("");
+}
+
+function onGalleryLinkClick(evt) {
+    evt.preventDefault()
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    onModal(evt);
+}
+
+function onModal(evt) {
+    const instance = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+        {
+        onShow: (instance) => {
+            galleryList.addEventListener("keydown", onEscPress)
+        },
+    });
+    instance.show()
+
+    function onEscPress(evt) {
+        if (evt.code === "Escape") {
+            instance.close()
+        }
+    }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Change code below this line
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+galleryList.addEventListener("click", onGalleryLinkClick);
+
+function createGalleryMarkup (gallery) {
+    return gallery.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}"/>
+                </a>`
+    }).join("");
+}
+
+function onGalleryLinkClick(evt) {
+    evt.preventDefault()
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+}
+new SimpleLightbox('.gallery a', { captions: true,  captionsData: "alt", captionDelay: 250,});
